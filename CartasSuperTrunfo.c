3f6a30ff2131b3230279bf91b8586f31bcf280c8
jcruzceli-alt <jcruzceli@gmail.com>
2025-10-08 00:06:43 +0000

    conclusao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,30 +17,70 @@ int main() {
     
   // Área para entrada de dados
 
-    printf ("Digite a letra referente do 1º Estado: ");
-    scanf("%s", estado1);
+    printf ("Digite a letra referente do primeiro Estado: "); scanf("%s", estado1);
 
-    printf ("Digite o código da 1ª Carta : ");
-    scanf("%s", cod_carta1);
+    printf ("Digite o código da primeira Carta..........: "); scanf("%s", cod_carta1);
 
-    printf ("Digite o nome da 1ª Cidade : ");
-    scanf("%s", cidade1);
+    printf ("Digite o nome da primeira Cidade...........: "); scanf("%s", cidade1);
 
-    printf ("População da 1ª Cidade : ");
-    scanf("%d",&populacao1);
+    printf ("População da 1ª Cidade.....................: "); scanf("%d",&populacao1);
 
+    printf ("Digite a área da Cidade....................: "); scanf("%f",&area1); 
+
+    printf ("Qual é o PIB da Cidade.....................: "); scanf("%f",&pib1);
+
+    printf("Quantos pontos turisticos existem na Cidade.: "); scanf("%d",&quant_pontos_tur1);
 
     printf ("\n");
     printf ("------------------------------------------------------- \n");
     printf ("\n");
 
-
   // Área para exibição dos dados da cidade
 
-      printf("A letra referente ao 1º Estado é: %s\n", estado1);
-      printf("O código da 1º Carta é: %s\n", cod_carta1);
-      printf("O Nome da 1ª Caidade é: %s\n", cidade1);
-      printf("A população da 1ª Caidade é: %d\n", populacao1);
+      printf("A letra referente ao primeiro Estado é..........: %s\n", estado1);
+      printf("O código da primeira Carta é....................: %s\n", cod_carta1);
+      printf("O Nome da primeira Caidade é....................: %s\n", cidade1);
+      printf("A população da primeira Cidade é................: %d\n", populacao1);
+      printf("A área da Cidade é..............................: %f\n", area1);
+      printf("O PIB da Cidade é...............................: %f\n", pib1);
+      printf("Quantos pontos turisticos existem na Cidade.....: %d\n", quant_pontos_tur1);
+
+ printf ("\n");
+    printf ("------------------------------------------------------- \n");
+    printf ("\n");
+
+        // Área para entrada de dados
+
+    printf ("Digite a letra referente do segundo Estado.: "); scanf("%s", estado2);
+
+    printf ("Digite o código da segunda Carta...........: "); scanf("%s", cod_carta2);
+
+    printf ("Digite o nome da segunda Cidade............: "); scanf("%s", cidade2);
+
+    
+    printf ("População da segunda Cidade................: "); scanf("%d",&populacao2);
+
+    printf ("Digite a área da Cidade....................: "); scanf("%f",&area2); 
+
+    printf ("Qual é o PIB da Cidade.....................: "); scanf("%f",&pib2);
+
+    printf("Quantos pontos turisticos existem na Cidade.: "); scanf("%d",&quant_pontos_tur2);
+
+    printf ("\n");
+    printf ("------------------------------------------------------- \n");
+    printf ("\n");
+
+// Área para exibição dos dados da cidade
+
+      printf("A letra referente ao segundo Estado é...........: %s\n", estado2);
+      printf("O código da primeira Carta é....................: %s\n", cod_carta2);
+      printf("O Nome da primeira Caidade é....................: %s\n", cidade2);
+      printf("A população da primeira Cidade é................: %d\n", populacao2);
+      printf("A área da Cidade é..............................: %f\n", area2);
+      printf("O PIB da Cidade é...............................: %f\n", pib2);
+      printf("Quantos pontos turisticos existem na Cidade.....: %d\n", quant_pontos_tur2);
+
+
 
 return 0;
 } 
